eaffcda82eaef9aaf0ba72eb800f78d45e887dd9
John Leszczynski <jrleszczynski@gmail.com>
2015-08-20 07:08:59 -0400

    use define for constants

--- chap_01/fahr_celsius_table.c
+++ chap_01/fahr_celsius_table.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+#define LOWER 0         /* lower limit of temp table */
+#define UPPER 300       /* upper limit */
+#define STEP  20        /* step size */
+
 /* print Fahrenheit-Celsius table
     for fahr = 0, 20, ..., 300 */
 main()
@@ -9,6 +13,6 @@ main()
     printf("Fahr    Cel\n");
     printf("----  ------\n");
 
-    for (fahr = 300; fahr >= 0; fahr = fahr - 20)
-        printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
+        printf("%4d  %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 }
